7571d89bd8b0631f635f4f56399ad1aa5b8c06e0
jagan <jaganrajagopal@outlook.com>
2023-08-03 23:33:18 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Jenkinswithdockercicd.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
